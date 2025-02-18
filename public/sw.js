d0e69d74b17e15e3f1e71fca8c241f995dd32e4f
cphil31 <cecilephilippe31@gmail.com>
2025-02-18 14:55:23 +0100

    first commit

---
+++ public/sw.js
@@ -0,0 +1,22 @@
+// public/sw.js
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/css/styles.css',
+  '/js/app.js',
+  '/images/logo.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
